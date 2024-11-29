201dd9e22fcad7cd96bff5e471153c2c0db36f6d
vikas.anand <vikas.anand@niveussolutions.com>
2024-11-30 01:42:42 +0530

    upd3324

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-const express = require('axios');
+const express = require('express');
 const axios = require('axios');
 
 const app = express();
 const SHEET_ID = '1oejo_52tgCkLdDeRDjjFywvmw4xA-s--m2dUZYYH3p8';
 
-// Define the route handler
-const handler = async (request, response) => {
+async function fetchFirstEntry(req, res) {
   const sheetUrl = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&range=A:A`;
 
   try {
     const response = await axios.get(sheetUrl);
     const firstEntry = response.data.split('\n')[0];
-    response.send(`First entry: ${firstEntry}`);
+    res.status(200).send(`First entry: ${firstEntry}`);
   } catch (error) {
-    response.status(500).send(`Error: ${error.message}`);
+    res.status(500).send(`Error: ${error.message}`);
   }
-};
+}
 
-// Export the handler for Vercel
-module.exports = handler;
+app.get('/', fetchFirstEntry);
+
+module.exports = (req, res) => {
+  app(req, res);
+};
