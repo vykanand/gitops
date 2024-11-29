421d53f2b17e6e5d74edf909704fa17fb78239b4
vikas.anand <vikas.anand@niveussolutions.com>
2024-11-30 01:31:59 +0530

    uo1

--- server.js
+++ server.js
@@ -6,33 +6,34 @@ const SHEET_ID = '1oejo_52tgCkLdDeRDjjFywvmw4xA-s--m2dUZYYH3p8';
 
 app.get('/', async (req, res) => {
   const emailToRemove = req.query.email;
+  res.send(emailToRemove)
   
-  if (!emailToRemove) {
-    return res.status(400).send('Email parameter is required');
-  }
+  // if (!emailToRemove) {
+  //   return res.status(400).send('Email parameter is required');
+  // }
 
-  const sheetUrl = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:json`;
+  // const sheetUrl = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:json`;
 
-  try {
-    // Fetch current data
-    const response = await axios.get(sheetUrl);
-    const data = response.data.substring(47).slice(0, -2);
-    const jsonData = JSON.parse(data);
+  // try {
+  //   // Fetch current data
+  //   const response = await axios.get(sheetUrl);
+  //   const data = response.data.substring(47).slice(0, -2);
+  //   const jsonData = JSON.parse(data);
     
-    // Process and filter data
-    const rows = jsonData.table.rows;
-    const updatedRows = rows.filter(row => row.c[0].v !== emailToRemove);
+  //   // Process and filter data
+  //   const rows = jsonData.table.rows;
+  //   const updatedRows = rows.filter(row => row.c[0].v !== emailToRemove);
     
-    // Update sheet with filtered data
-    const updateUrl = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/edit#gid=0`;
-    await axios.post(updateUrl, {
-      data: updatedRows
-    });
+  //   // Update sheet with filtered data
+  //   const updateUrl = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/edit#gid=0`;
+  //   await axios.post(updateUrl, {
+  //     data: updatedRows
+  //   });
 
-    res.send(`Email ${emailToRemove} removed successfully`);
-  } catch (error) {
-    res.status(500).send(`Error: ${error.message}`);
-  }
+  //   res.send(`Email ${emailToRemove} removed successfully`);
+  // } catch (error) {
+  //   res.status(500).send(`Error: ${error.message}`);
+  // }
 });
 
 module.exports = (req, res) => {
