9cae91a849c128672883b0b895ac812c0a6a215b
vikas.anand <vikas.anand@niveussolutions.com>
2024-11-30 02:50:12 +0530

    upfs

--- server.js
+++ server.js
@@ -6,19 +6,16 @@ const app = express();
 const APPROVED_EMAILS = 'approved.txt';
 const BANNED_EMAILS = 'banned.txt';
 
-// Create files if they don't exist
 [APPROVED_EMAILS, BANNED_EMAILS].forEach(file => {
   if (!fs.existsSync(file)) {
     fs.writeFileSync(file, '');
   }
 });
 
-// Serve static HTML
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// API endpoints
 app.get('/api/emails', (req, res) => {
   const approved = fs.readFileSync(APPROVED_EMAILS, 'utf8').split('\n').filter(Boolean);
   const banned = fs.readFileSync(BANNED_EMAILS, 'utf8').split('\n').filter(Boolean);
@@ -33,13 +30,16 @@ app.get('/api/move', (req, res) => {
   let fromEmails = fs.readFileSync(fromFile, 'utf8').split('\n').filter(Boolean);
   let toEmails = fs.readFileSync(toFile, 'utf8').split('\n').filter(Boolean);
 
+  // Remove from both lists to prevent duplicates
   fromEmails = fromEmails.filter(e => e !== email);
-  if (!toEmails.includes(email)) {
-    toEmails.push(email);
-  }
+  toEmails = toEmails.filter(e => e !== email);
+  
+  // Add to target list
+  toEmails.push(email);
 
-  fs.writeFileSync(fromFile, fromEmails.join('\n'));
-  fs.writeFileSync(toFile, toEmails.join('\n'));
+  // Save unique emails only
+  fs.writeFileSync(fromFile, [...new Set(fromEmails)].join('\n'));
+  fs.writeFileSync(toFile, [...new Set(toEmails)].join('\n'));
 
   res.json({ message: `Email ${email} moved to ${to} list` });
 });
